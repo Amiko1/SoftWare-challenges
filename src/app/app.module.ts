0c6ba959c0310504398c9d4ec838eab219c99ea2
amirani <amirani>
2024-06-28 12:18:56 +0400

    setup: router for job-experience

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { JobExperienceFormComponent } from './job-experience-form/job-experience-form.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'job-experience', component: JobExperienceFormComponent },
+];
 
 @NgModule({
   declarations: [AppComponent, JobExperienceFormComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(routes)],
   providers: [provideAnimationsAsync()],
   bootstrap: [AppComponent],
 })
