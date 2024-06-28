414d883dc799d486a8b9760631cc4625976a0481
amirani <amirani>
2024-06-28 14:33:52 +0400

    feat: create formGroup and addJob functional

--- src/app/job-experience-form/job-experience-form.component.ts
+++ src/app/job-experience-form/job-experience-form.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-job-experience-form',
   templateUrl: './job-experience-form.component.html',
-  styleUrl: './job-experience-form.component.css'
+  styleUrls: ['./job-experience-form.component.css'],
 })
 export class JobExperienceFormComponent {
+  jobExperienceForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.jobExperienceForm = this.fb.group({
+      jobs: this.fb.array([]),
+    });
+  }
+
+  get jobs(): FormArray {
+    return this.jobExperienceForm.get('jobs') as FormArray;
+  }
+
+  addJob() {
+    const controls = {
+      companyName: ['', Validators.required],
+      companyWebsite: [
+        '',
+        [Validators.required, Validators.pattern('https?://.+')],
+      ],
+      companyDescription: ['', Validators.required],
+      positions: this.fb.array([]),
+    };
+
+    const jobForm = this.fb.group(controls);
+    this.jobs.push(jobForm);
+  }
 }
