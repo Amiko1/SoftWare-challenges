cebc3812c24d9530e3a3b7084653094da9ffd004
amirani <amirani>
2024-06-29 11:18:20 +0400

    update: use new faster algorithm for leventheiDistance

--- src/app/utils/levenshteinDistance.ts
+++ src/app/utils/levenshteinDistance.ts
@@ -1,30 +1,27 @@
-export function levenshteinDistance(
-  str1: string,
-  str2: string,
-  m: number,
-  n: number
-): number {
-  if (m === 0) {
-    return n;
-  }
+export function levenshteinDistance(str1: string, str2: string): number {
+  const m = str1.length;
+  const n = str2.length;
 
-  if (n === 0) {
-    return m;
-  }
-
-  if (str1[m - 1] === str2[n - 1]) {
-    return levenshteinDistance(str1, str2, m - 1, n - 1);
-  }
-
-  return (
-    1 +
-    Math.min(
-      // Insert
-      levenshteinDistance(str1, str2, m, n - 1),
-      // Remove
-      levenshteinDistance(str1, str2, m - 1, n),
-      // Replace
-      levenshteinDistance(str1, str2, m - 1, n - 1)
-    )
+  const dp: number[][] = Array.from({ length: m + 1 }, () =>
+    Array(n + 1).fill(0)
   );
+
+  for (let i = 0; i <= m; i++) {
+    dp[i][0] = i;
+  }
+  for (let j = 0; j <= n; j++) {
+    dp[0][j] = j;
+  }
+
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      if (str1[i - 1] === str2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1];
+      } else {
+        dp[i][j] = 1 + Math.min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]);
+      }
+    }
+  }
+
+  return dp[m][n];
 }
